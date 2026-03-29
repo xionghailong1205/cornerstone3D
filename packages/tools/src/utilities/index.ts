2aa021012b59a03d0c99a2ca620935c20b69da15
xionghailong <xionghailongk@gmail.com>
2026-03-29 23:27:13 +0800

    备份 3.29

--- packages/tools/src/utilities/index.ts
+++ packages/tools/src/utilities/index.ts
@@ -59,6 +59,7 @@ import setAnnotationLabel from './setAnnotationLabel';
 import { moveAnnotationToViewPlane } from './moveAnnotationToViewPlane';
 import getOrCreateImageVolume from './segmentation/getOrCreateImageVolume';
 import * as usFanExtraction from '../tools/annotation/UltrasoundPleuraBLineTool/utils/fanExtraction';
+import InterpolationManager from './segmentation/InterpolationManager/InterpolationManager';
 
 export {
   math,
@@ -110,4 +111,5 @@ export {
   setAnnotationLabel,
   moveAnnotationToViewPlane,
   getOrCreateImageVolume,
+  InterpolationManager,
 };
